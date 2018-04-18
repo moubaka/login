4ca255ebbc1305eee383ce2c7fcb3e90781e8873
moubaka <38161312+moubaka@users.noreply.github.com>
2018-04-18 16:28:34 +0400

    Add files via upload

--- firebaseAuth.js
+++ firebaseAuth.js
@@ -1,2 +1,11 @@
+"use-strict";
 
-
+var config = {
+    apiKey: "AIzaSyC_M-c_7pQI-XXEaB8ACDezSlGzbLbOfN8",
+    authDomain: "test-1433b.firebaseapp.com",
+    databaseURL: "https://test-1433b.firebaseio.com",
+    projectId: "test-1433b",
+    storageBucket: "test-1433b.appspot.com",
+    messagingSenderId: "683916710229"
+};
+firebase.initializeApp(config);
---
+++ script.js
@@ -0,0 +1,57 @@
+"use-strict";
+
+
+
+function login() {
+    const email = document.getElementById("signin-email").value,
+        password = document.getElementById("signin-password").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function (response) {
+            debugger;
+            console.log("response SignIn: " + response);
+        })
+        .catch(function (error) {
+            console.log('error SignIn: ' + error);
+        });
+}
+
+function register() {
+    const email = document.getElementById("signup-email").value,
+        password = document.getElementById("signup-password").value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function (response) {
+            debugger
+            console.log("response SignUp: ", response);
+        })
+        .catch(function (error) {
+            console.log("error SignUp: ", error)
+        });
+
+}
+
+function googleLogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (response) {
+
+        console.log("Google Login response: ", response);
+
+    }).catch(function (error) {
+
+        console.log("Google Login error: ", error)
+    });
+
+}
+
+function fbLogin() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (response) {
+
+        console.log('FB Login response: ', response);
+
+    }).catch(function (error) {
+        console.log('FB login error', error);
+
+    });
+}
